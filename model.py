36d19fad655a485ff5830780e90f0a9dd15888ff
Bernhard Langwallner <bernhardlangwallner@gmail.com>
2018-08-19 18:20:16 +0200

    committing first prototype

---
+++ model.py
@@ -0,0 +1,31 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class QNetwork(nn.Module):
+    """Actor (Policy) Model."""
+
+    def __init__(self, state_size, action_size, seed):
+        """Initialize parameters and build model.
+        Params
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+            seed (int): Random seed
+        """
+        super(QNetwork, self).__init__()
+        self.seed = torch.manual_seed(seed)
+        hidden_size = 64
+        self.fc1 = nn.Linear(state_size,hidden_size)
+        self.fc2 = nn.Linear(hidden_size, hidden_size/2)      
+        self.fc3 = nn.Linear(hidden_size, action_size)      
+        
+
+    def forward(self, state):
+        """Build a network that maps state -> action values."""
+        x = self.fc1(state)
+        x = F.relu(x)
+        x = self.fc2(x)
+        x = F.relu(x)
+        x = self.fc3(x)
+        return x
